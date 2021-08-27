7a01198c95fab81e9d96aa471448952e19254288
Francis Angelo Reyes <81660334+Lupage@users.noreply.github.com>
2021-08-27 15:24:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,19 @@ def get_word_count(argument):
 url_input = st.file_uploader(label="Upload a .TXT file with 50 URLs only. No HEADERS.", type='txt', accept_multiple_files=False)
 
 if url_input:
-  bytes_data = StringIO(url_input.getvalue().decode("utf-8"))
-  url_list = [element.strip() for element in bytes_data]
-  if len(url_list) > 50:
-    st.warning("Upload 50 URLs only")
-  else:
-  	word_count = [get_word_count(element) for element in url_list]
-    results = zip(url_list, word_count)
-    df = pd.DataFrame(results)
-    df.columns = ["URL", "Word Count"]
-    df.index = df.index + 1
-  	st.table(df)
-  	csv = df.to_csv()
-  	b64 = base64.b64encode(csv.encode()).decode()
-  	st.markdown('### **⬇️ Download output CSV File **')
-  	href = f"""<a href="data:file/csv;base64,{b64}">Download CSV File</a> (Right-click and save as "filename.csv". Don't left-click)"""
-  	st.markdown(href, unsafe_allow_html=True)
+    bytes_data = StringIO(url_input.getvalue().decode("utf-8"))
+    url_list = [element.strip() for element in bytes_data]
+    if len(url_list) > 50:
+        st.warning("Upload 50 URLs only")
+    else:
+        word_count = [get_word_count(element) for element in url_list]
+        results = zip(url_list, word_count)
+        df = pd.DataFrame(results)
+        df.columns = ["URL", "Word Count"]
+        df.index = df.index + 1
+        st.table(df)
+        csv = df.to_csv()
+        b64 = base64.b64encode(csv.encode()).decode()
+        st.markdown('### **⬇️ Download output CSV File **')
+        href = f"""<a href="data:file/csv;base64,{b64}">Download CSV File</a> (Right-click and save as "filename.csv". Don't left-click)"""
+        st.markdown(href, unsafe_allow_html=True)
