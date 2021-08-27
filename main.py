d3592c63677cbea5bc385751c9b55831fb47faac
Francis Angelo Reyes <81660334+Lupage@users.noreply.github.com>
2021-08-27 17:58:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import pandas as pd
 import requests
 import base64
 
+#Streamlit start design
 st.set_page_config(page_title="Word Counter")
+st.title("***Word Counter***", anchor=None)
 
+#Word count function
 def get_word_count(argument):
     full_url = argument
     page_source = requests.get(full_url).text
@@ -15,18 +18,28 @@ def get_word_count(argument):
     word_count = len(str(paragraph_list).split())
     return word_count
 
-st.title("***Word Counter***", anchor=None)
+#Variables
+url_input = st.file_uploader(label="Upload a .TXT file with up to 50 URLs only. No HEADERS.", type='txt', accept_multiple_files=False)
+count = 0
+my_bar = st.progress(count)
+word_count = []
 
-url_input = st.file_uploader(label="Upload a .TXT file with 50 URLs only. No HEADERS.", type='txt', accept_multiple_files=False)
-
+#Streamlit logic
 if url_input:
     bytes_data = StringIO(url_input.getvalue().decode("utf-8"))
     url_list = [element.strip() for element in bytes_data]
+    
     if len(url_list) > 50:
-        st.warning("Upload 50 URLs only")
-    else:
-        word_count = [get_word_count(element) for element in url_list]
-        results = zip(url_list, word_count)
+        st.warning("Upload up to 50 URLs only")
+        else:
+            #Build word count list and streamlit progress bar
+            for element in url_list:
+                word_count.append(get_word_count(element))
+                count += 1 / len(url_list)
+                my_bar.progress(round(count,3))
+                
+        #DataFrame creation
+        results = list(zip(url_list, word_count))
         df = pd.DataFrame(results)
         df.columns = ["URL", "Word Count"]
         df.index = df.index + 1
