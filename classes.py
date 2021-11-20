1960bb8150f0a3f5701d16a7c78c0a3ff9c36ac5
Francis Angelo Reyes <81660334+Lupage@users.noreply.github.com>
2021-11-20 09:25:44 +0800

    Update classes.py

--- classes.py
+++ classes.py
@@ -16,13 +16,15 @@ class Page:
         soup = BeautifulSoup(page_source, 'html.parser')
         meta_description = soup.find('meta',attrs={'name':'description'})["content"]
         return meta_description
-
-    def word_count(self):
-    	page_source = requests.get(self.url).text
-    	soup = BeautifulSoup(page_source, 'html.parser')
-    	paragraph_list = [element.text for element in soup.find_all('p')]
-    	word_count = len(str(paragraph_list).split())
-    	return word_count
+    
+    def get_word_count(argument):
+        full_url = argument
+        page_source = requests.get(full_url).text
+        soup = BeautifulSoup(page_source, 'lxml')
+        paragraph_list = [element.text for element in soup.find_all('p')]
+        list_list = [element.text for element in soup.find_all('li')]
+        word_count = len(str(paragraph_list).split()) + len(str(list_list).split())
+        return word_count
 
     def content(self):
         page_source = requests.get(self.url).text
